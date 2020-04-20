476322247c7069df7a71be336940c59a09296a10
user.name <user.email>
2020-04-20 22:25:47 +0700

    V 1.0

---
+++ app/src/main/java/com/example/tranvanty_lifecycle_demo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.tranvanty_lifecycle_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("LifeCycle","Create called");
+        Toast.makeText(getApplicationContext(),"OnCreate ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle","onStart called");
+        Toast.makeText(getApplicationContext(),"OnStart ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle","onRestart called");
+        Toast.makeText(getApplicationContext(),"ReStart ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCycle","On Resume called");
+        Toast.makeText(getApplicationContext(),"Resume ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle","onPause called");
+        Toast.makeText(getApplicationContext(),"Pause ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCycle","onStop called");
+        Toast.makeText(getApplicationContext(),"Stop ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle","onDestroy called");
+        Toast.makeText(getApplicationContext(),"Destroy ", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
